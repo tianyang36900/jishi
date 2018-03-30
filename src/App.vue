fbc6400e8421bf2fe79856bbd25e0ee03680029d
tianyang <1170573444@qq.com>
2018-03-30 11:54:05 +0800

    Signed-off-by: tianyang <1170573444@qq.com>

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="app">
+		<!--<img src="./assets/logo.png">-->
+		<Abc />
+		<!--<Sa/>-->
+		<!--<HelloWorld msg="Welcome to beijing" />-->
+	</div>
+</template>
+
+<script>
+	//	import Sa from './components/HelloWorld.vue'
+	//	import Title from './components/Title.vue'
+	//	import Dianji from './components/Dianji.vue'
+	//	import Xuanxiangka from './components/Xuanxiangka.vue'
+	//	import Xuanxiangka1 from './components/Xuanxiangka1.vue'
+//		import Xinwenzixun from './components/Xinwenzixun.vue'
+//	import News from './components/News.vue'
+import Mysql from './components/Mysql.vue'
+	//	import Xialacaidan from './components/Xialacaidan.vue'
+	export default {
+		name: 'app',
+		components: {
+			//			Abc: Title,
+			//			Abc: Dianji,
+			//			Abc: Xuanxiangka,
+			//			Abc: Xuanxiangka1,
+			//			Abc: Xinwenzixun,
+//			Abc: news,
+				Abc:Mysql,
+			//			Sa
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		/*text-align: center;*/
+		color: #2c3e50;
+		/*margin-top: 60px;*/
+	}
+</style>
---
+++ src/components/Mysql.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<div>
+			<div id="box">
+				<button>添加</button>
+				<table>
+					<tr>
+						<td>学号</td>
+						<td>姓名</td>
+						<td>年龄</td>
+					</tr>
+					<tr v-for="i in arr">
+						<td>{{i.id}}</td>
+						<td>{{i.tit}}</td>
+						<td>{{i.con}}</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				arr: []
+			}
+		},
+		created() {
+			this.$http.get('http://localhost:8000')
+				.then((e) => this.arr = e.body)
+		}
+
+	}
+</script>
+<style>
+	#box {
+		width: 500px;
+		height: 300px;
+		border: 1px solid #2C3E50;
+	}
+</style>
